b92eb40f1e58c836dcb16df2a9839ca18fd3f0f1
takuowake <takuowake@gmail.com>
2023-08-08 20:17:21 +0900

    toppageのUI

--- lib/pages/top_page.dart
+++ lib/pages/top_page.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/model/user.dart';
 import 'package:flutter/material.dart';
 
 class TopPage extends StatefulWidget {
@@ -8,8 +9,47 @@ class TopPage extends StatefulWidget {
 }
 
 class _TopPageState extends State<TopPage> {
+  List<User> userList = [
+    User(name: '田中', uid: 'abc',
+        imagePath: 'https://cdn-images-1.medium.com/max/1200/1*ilC2Aqp5sZd1wi0CopD1Hw.png',
+        lastMessage: 'こんにちは'),
+    User(name: '佐藤', uid: 'abcd',
+        imagePath: 'https://cdn-images-1.medium.com/max/1200/1*ilC2Aqp5sZd1wi0CopD1Hw.png',
+        lastMessage: 'こんばんわ'),
+  ];
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('チャットアプリ')),
+      body: ListView.builder(
+        itemCount: userList.length,
+        itemBuilder: (context, index) {
+          return SizedBox(
+            height: 70,
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: CircleAvatar(
+                    radius: 30,
+                    backgroundImage: userList[index].imagePath == null
+                      ? null
+                      : NetworkImage(userList[index].imagePath!),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(userList[index].name, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                    Text(userList[index].lastMessage, style: TextStyle(color: Colors.grey),),
+                  ],
+                )
+              ],
+            ),
+          );
+        }
+      ),
+    );
   }
 }
