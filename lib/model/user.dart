8230c8db9ed6a446e2ebc8431653fbb5f1f2923e
takuowake <takuowake@gmail.com>
2023-08-08 19:52:05 +0900

    userの定義

---
+++ lib/model/user.dart
@@ -0,0 +1,14 @@
+class User {
+  String name;
+  String uid;
+  String? imagePath;
+  String lastMessage;
+
+  User({
+    required this.name,
+    required this.uid,
+    this.imagePath,
+    this.lastMessage = '',
+
+  });
+}
