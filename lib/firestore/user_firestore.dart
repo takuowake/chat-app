89867cba3bdc670a4da00e7630a7b590f560c1f9
takuowake <takuowake@gmail.com>
2023-08-11 15:07:51 +0900

    自分のアカウントを取得

--- lib/firestore/user_firestore.dart
+++ lib/firestore/user_firestore.dart
@@ -1,10 +1,13 @@
+import 'package:chat_app/firestore/room_firestore.dart';
+import 'package:chat_app/model/user.dart';
+import 'package:chat_app/utils/shared_prefs.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class UserFirestore {
   static final FirebaseFirestore _firebaseFirestoreInstance = FirebaseFirestore.instance;
   static final _userCollection = _firebaseFirestoreInstance.collection('user');
 
-  static Future<String?> createUser() async {
+  static Future<String?> insertNewAccount() async {
     try {
       final newDoc = await _userCollection.add({
         'name': 'waketakuo',
@@ -18,12 +21,36 @@ class UserFirestore {
     }
   }
 
+  static Future<void> createUser() async {
+    final myUid = await insertNewAccount();
+    if(myUid != null) {
+      await RoomFirestore.createRoom(myUid);
+      await SharedPrefs.setUid(myUid);
+    }
+  }
+
   static Future<List<QueryDocumentSnapshot>?> fetchUsers() async {
     try {
       final snapshot = await _userCollection.get();
       return snapshot.docs;
     } catch(e) {
       print('ユーザー情報の取得失敗: $e');
+    }
+  }
+
+  static Future<User?> fetchMyProfile() async {
+    try {
+      String uid = SharedPrefs.fetchUid()!;
+      final myProfile = await _userCollection.doc(uid).get();
+      User user = User(
+        name: myProfile.data()!['name'],
+        imagePath: myProfile.data()!['image_path'],
+        uid: uid,
+      );
+      return user;
+    } catch(e) {
+      print('自分のユーザー情報の取得失敗: $e');
+      return null;
     }
   }
 }
