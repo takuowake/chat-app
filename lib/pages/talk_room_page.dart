4d95db68e70dc1a0aed714bd3152ffba9bd8438a
takuowake <takuowake@gmail.com>
2023-08-12 16:23:18 +0900

    リストタップ次にそのユーザーとのトークルームを表示

--- lib/pages/talk_room_page.dart
+++ lib/pages/talk_room_page.dart
@@ -1,82 +1,78 @@
+import 'package:chat_app/firestore/room_firestore.dart';
 import 'package:chat_app/model/message.dart';
+import 'package:chat_app/model/talk_room.dart';
+import 'package:chat_app/utils/shared_prefs.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart' as intl;
 
 class TalkRoomPage extends StatefulWidget {
-  final String name;
-  const TalkRoomPage(this.name, {Key? key}) : super(key: key);
+  final TalkRoom talkRoom;
+  const TalkRoomPage(this.talkRoom, {Key? key}) : super(key: key);
 
   @override
   State<TalkRoomPage> createState() => _TalkRoomPageState();
 }
 
 class _TalkRoomPageState extends State<TalkRoomPage> {
-  List<Message> messageList = [
-    Message(message: 'aiu', isMe: true, sendTime: DateTime(2023, 1, 1, 12, 00)),
-    Message(message: 'aiueo', isMe: false, sendTime: DateTime(2023, 1, 1, 12, 00)),
-    Message(message: 'aiueokakikukekosasisusedf;jdjfa;dsjkf;safjdas;asotatitutetonaninunenohahihuheho', isMe: false, sendTime: DateTime(2023, 1, 1, 12, 00)),
-    Message(message: 'aiu', isMe: true, sendTime: DateTime(2023, 1, 1, 12, 00)),
-    Message(message: 'aiueo', isMe: false, sendTime: DateTime(2023, 1, 1, 12, 00)),
-    Message(message: 'aiueokakikukekosasisusedf;jdjfa;dsjkf;safjdas;asotatitutetonaninunenohahihuheho', isMe: false, sendTime: DateTime(2023, 1, 1, 12, 00)),
-    Message(message: 'aiu', isMe: true, sendTime: DateTime(2023, 1, 1, 12, 00)),
-    Message(message: 'aiueo', isMe: false, sendTime: DateTime(2023, 1, 1, 12, 00)),
-    Message(message: 'aiueokakikukekosasisusedf;jdjfa;dsjkf;safjdas;asotatitutetonaninunenohahihuheho', isMe: false, sendTime: DateTime(2023, 1, 1, 12, 00)),
-    Message(message: 'aiu', isMe: true, sendTime: DateTime(2023, 1, 1, 12, 00)),
-    Message(message: 'aiueo', isMe: false, sendTime: DateTime(2023, 1, 1, 12, 00)),
-    Message(message: 'aiueokakikukekosasisusedf;jdjfa;dsjkf;safjdas;asotatitutetonaninunenohahihuheho', isMe: false, sendTime: DateTime(2023, 1, 1, 12, 00)),
-    Message(message: 'aiu', isMe: true, sendTime: DateTime(2023, 1, 1, 12, 00)),
-    Message(message: 'aiueo', isMe: false, sendTime: DateTime(2023, 1, 1, 12, 00)),
-    Message(message: 'aiueokakikukekosasisusedf;jdjfa;dsjkf;safjdas;asotatitutetonaninunenohahihuheho', isMe: false, sendTime: DateTime(2023, 1, 1, 12, 00)),
-    Message(message: 'aiu', isMe: true, sendTime: DateTime(2023, 1, 1, 12, 00)),
-    Message(message: 'aiueo', isMe: false, sendTime: DateTime(2023, 1, 1, 12, 00)),
-    Message(message: 'aiueokakikukekosasisusedf;jdjfa;dsjkf;safjdas;asotatitutetonaninunenohahihuheho', isMe: false, sendTime: DateTime(2023, 1, 1, 12, 00)),
-    Message(message: 'aiu', isMe: true, sendTime: DateTime(2023, 1, 1, 12, 00)),
-    Message(message: 'aiueo', isMe: false, sendTime: DateTime(2023, 1, 1, 12, 00)),
-    Message(message: 'aiueokakikukekosasisusedf;jdjfa;dsjkf;safjdas;asotatitutetonaninunenohahihuheho', isMe: false, sendTime: DateTime(2023, 1, 1, 12, 00)),
-  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlue,
-      appBar: AppBar(title: Text(widget.name)),
+      appBar: AppBar(title: Text(widget.talkRoom.talkUser.name)),
       body: Stack(
-        alignment: Alignment.bottomCenter,
         children: [
-          Padding(
-            padding: const EdgeInsets.only(bottom: 60.0),
-            child: ListView.builder(
-              physics: RangeMaintainingScrollPhysics(),
-              shrinkWrap: true,
-              reverse: true,
-              itemCount: messageList.length,
-              itemBuilder: (context, index) {
+          StreamBuilder<QuerySnapshot>(
+            stream: RoomFirestore.fetchMessageSnapshot(widget.talkRoom.roomId),
+            builder: (context, snapshot) {
+              if(snapshot.hasData) {
                 return Padding(
-                  padding: EdgeInsets.only(top: 10, left: 10, right: 10, bottom: index == 0 ? 10 : 0),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    textDirection: messageList[index].isMe ? TextDirection.rtl : TextDirection.ltr,
-                    children: [
-                      Container(
-                        constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width * 0.6),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: messageList[index].isMe == true
-                              ? Colors.green
-                              : Colors.white,
-                        ),
-                        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 6),
-                        child: Text(messageList[index].message)
-                      ),
-                      Text(intl.DateFormat('HH:mm').format(messageList[index].sendTime))
-                    ],
+                  padding: const EdgeInsets.only(bottom: 60.0),
+                  child: ListView.builder(
+                      physics: RangeMaintainingScrollPhysics(),
+                      shrinkWrap: true,
+                      reverse: true,
+                      itemCount: snapshot.data!.docs.length,
+                      itemBuilder: (context, index) {
+                        final doc = snapshot.data!.docs[index];
+                        final Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+                        final Message message = Message(
+                            message: data['message'],
+                            isMe: SharedPrefs.fetchUid() == data['sender_id'],
+                            sendTime: data['send_time'],
+                        );
+                        return Padding(
+                          padding: EdgeInsets.only(top: 10, left: 10, right: 10, bottom: index == 0 ? 10 : 0),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            textDirection: message.isMe ? TextDirection.rtl : TextDirection.ltr,
+                            children: [
+                              Container(
+                                  constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width * 0.6),
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    color: message.isMe == true
+                                        ? Colors.green
+                                        : Colors.white,
+                                  ),
+                                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                                  child: Text(message.message)
+                              ),
+                              Text(intl.DateFormat('HH:mm').format(message.sendTime.toDate()))
+                            ],
+                          ),
+                        );
+                      }
                   ),
                 );
+              } else {
+                return const Center(child: Text('メッセージがありません'),);
               }
-            ),
+            }
           ),
           Column(
-            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.end,
             children: [
               Container(
                 color: Colors.white,
