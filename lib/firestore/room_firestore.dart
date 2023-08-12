4f9e5bfb46492db0367bef0c88266d63e6955cea
takuowake <takuowake@gmail.com>
2023-08-12 16:45:49 +0900

    トークリストにlastmessageを表示

--- lib/firestore/room_firestore.dart
+++ lib/firestore/room_firestore.dart
@@ -66,6 +66,10 @@ class RoomFirestore {
         'sender_id': SharedPrefs.fetchUid(),
         'send_time': Timestamp.now(),
       });
+
+      await _roomCollection.doc(roomId).update({
+        'last_message': message
+      });
     } catch(e) {
       print('メッセージの送信失敗: $e');
     }
