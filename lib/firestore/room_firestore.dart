f968bb504959efff382ea9ad19fb5e284f840256
takuowake <takuowake@gmail.com>
2023-08-12 16:03:49 +0900

    ルーム情報をもとにユーザーリストを表示

--- lib/firestore/room_firestore.dart
+++ lib/firestore/room_firestore.dart
@@ -7,6 +7,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class RoomFirestore {
   static final FirebaseFirestore _firebaseFirestoreInstance = FirebaseFirestore.instance;
   static final _roomCollection = _firebaseFirestoreInstance.collection('room');
+  static final joinedRoomSnapshot = _roomCollection.where('joined_user_ids', arrayContains: SharedPrefs.fetchUid()).snapshots();
 
   static Future<void> createRoom(String myUid) async {
     try {
@@ -24,30 +25,32 @@ class RoomFirestore {
     }
   }
 
-  static Future<void> fetchJoinedRooms() async {
+  static Future<List<TalkRoom>?> fetchJoinedRooms(QuerySnapshot snapshot) async {
     try {
       String myUid = SharedPrefs.fetchUid()!;
-      final snapshot = await _roomCollection.where('joined_user_ids', arrayContains: myUid).get();
       List<TalkRoom> talkRooms = [];
       for(var doc in snapshot.docs) {
-        List<dynamic> userIds = doc.data()['joined_user_ids'];
+        Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+        List<dynamic> userIds = data['joined_user_ids'];
         late String talkUserUid;
         for(var id in userIds) {
           if(id == myUid) continue;
           talkUserUid = id;
         }
         User? talkUser = await UserFirestore.fetchProfile(talkUserUid);
-        if(talkUser == null) return;
+        if(talkUser == null) return null;
         final talkRoom = TalkRoom(
           roomId: doc.id,
           talkUser: talkUser,
-          lastMessage: doc.data()['last_message'],
+          lastMessage: data['last_message'],
         );
         talkRooms.add(talkRoom);
       }
       print(talkRooms.length);
+      return talkRooms;
     } catch(e) {
       print('参加しているルームの取得失敗: $e');
+      return null;
     }
   }
 }
